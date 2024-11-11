78e4f3404f5a0a0bf1b3e015dba1b8c114a666e0
Rashidul Islam Alvi <rashidul537@gmail.com>
2024-11-12 02:52:32 +0600

    feat: create nabvar-mobile

---
+++ components/navbar-mobile.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Button } from "./ui/button";
+import Logo from "./logo";
+
+interface MenuItem {
+  id: number;
+  label: string;
+  link: string;
+}
+
+interface NavbarMobileProps {
+  menuItems: MenuItem[];
+}
+const NavbarMobile = ({ menuItems }: NavbarMobileProps) => {
+  return (
+    <div className="p-6 h-full border-r flex items-center justify-between flex-col gap-10">
+      <div>
+        <Logo />
+      </div>
+
+      <ul className="flex flex-col items-center gap-10 ">
+        {menuItems.map((item) => (
+          <li key={item.id}>
+            <a
+              href={item.link}
+              className="text-white text-3xl hover:text-gray-500"
+            >
+              {item.label}
+            </a>
+          </li>
+        ))}
+      </ul>
+
+      <Button
+        variant="mustardYellow"
+        className="h-[44px] w-[157px] text-[16px]"
+      >
+        Book a table
+      </Button>
+    </div>
+  );
+};
+
+export default NavbarMobile;
