37645d11242673adfca3a57ce17e84951160e2ef
Rashidul Islam Alvi <rashidul537@gmail.com>
2024-11-12 08:24:05 +0600

    fix : issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Soudi Cuisine - Authenticated Place",
@@ -17,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <Navbar />
 
         {children}
